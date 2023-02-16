e4c04b9d930cbd7b97b55acbf7d3ff4c59b5c36b
J.Javier Reig <jjrp267@gmail.com>
2023-02-16 21:03:59 +0100

    add locale

--- symfony/src/Controller/HomeController.php
+++ symfony/src/Controller/HomeController.php
@@ -5,14 +5,18 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(TranslatorInterface $translator): Response
     {
+
+        $translated = $translator->trans('Translate me symfony');
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'translated' => $translated
         ]);
     }
 }
