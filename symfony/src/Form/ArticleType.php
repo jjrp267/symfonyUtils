fa420e913c22dd18bb9e7c439c6c98c4f280b74f
J.Javier Reig <jjrp267@gmail.com>
2023-02-16 19:33:22 +0100

    templates fixed

--- symfony/src/Form/ArticleType.php
+++ symfony/src/Form/ArticleType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\Article;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,11 +14,21 @@ class ArticleType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('description')
-            ->add('writter')
-            ->add('language')
-            ->add('date')
+            ->add('name', TextType::class,[
+                'label' => 'Nombre',
+            ])
+            ->add('Description', TextType::class,[
+                'label' => 'Descripción',
+            ])
+            ->add('writter', TextType::class,[
+                'label' => 'Escritor',
+            ])
+            ->add('language', TextType::class,[
+                'label' => 'Lenguaje',
+            ])
+            ->add('date', DateType::class, [
+                'label' => 'Fecha',
+            ])
         ;
     }
 
